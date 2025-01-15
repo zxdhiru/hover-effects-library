35c96835a7f1bc93930733a66f91179e58a3e217
Dhirendra Kumar Rana <dhiru@alljobguider.in>
2025-01-15 16:54:22 +0000

    Initialize empty vite react project with tailwindcss setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Card from "./components/Card";
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    <Card />
+    </div>
+  )
+}
---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>Hello</div>
+  )
+}
+
+export default Card
