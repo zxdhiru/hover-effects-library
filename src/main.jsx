5f3c419581e3d1b9f7e01513bf3133f21625474f
Dhirendra Kumar Rana <dhiru@alljobguider.in>
2025-01-15 18:17:23 +0000

    Implement routing with React Router for main application

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router"; // Note: Updated import path for React Router
+import SingleCard from "./components/SingleCard";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/effect/:effectName" element={<SingleCard />} />
+    </Routes>
+  </BrowserRouter>
+);
