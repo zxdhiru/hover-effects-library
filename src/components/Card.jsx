93dc3c8e36b8908aa16dd01e4502f6d12e802069
Dhirendra Kumar Rana <dhiru@alljobguider.in>
2025-01-15 18:17:33 +0000

    Refactor Card component to include routing and dynamic effect name display

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router';
 
-const Card = () => {
+const Card = ({effectName, className}) => {
   return (
-    <div>Hello</div>
-  )
-}
+    <div className={`bg-blue-800 rounded-lg p-6 flex justify-center items-center max-w-sm h-32 text-xl font-semibold border border-gray-800/25 cursor-pointer ${className}`}>
+      <Link to={`/effect/${effectName}`} className='h-full w-full flex justify-center items-center'>
+        {effectName}
+      </Link>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
