6f88551993322345caf7640514d4e64908c16af0
Dhirendra Kumar Rana <dhiru@alljobguider.in>
2025-01-15 18:39:02 +0000

    Add footer with creator credit and heart icon to main layout

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import SingleCard from "./components/SingleCard";
 import App from "./App";
 import "./index.css";
 import Navbar from "./components/Navbar";
+import { FaHeart } from "react-icons/fa";
 
 
 const root = document.getElementById("root");
@@ -12,9 +13,13 @@ const root = document.getElementById("root");
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
   <Navbar />
+  <main className="min-h-screen pt-10 bg-gradient-to-r from-purple-500 to-indigo-500 ">
+
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/effect/:effectName" element={<SingleCard />} />
     </Routes>
+      <p className="flex items-center text-center w-full justify-center py-4 gap-2 ">Create with <FaHeart/> by <a href="https://www.instagram.com/zxdhiru"><b className="hover:text-red-400">Dhiru</b></a></p>
+  </main>
   </BrowserRouter>
 );
